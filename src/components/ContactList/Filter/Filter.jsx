724c55a822798235f210bb2501eb58cd40b4cafc
SergeyFullstack <111132907+SergeyFullstack@users.noreply.github.com>
2023-07-08 00:21:30 +0300

    create hw-07

---
+++ src/components/ContactList/Filter/Filter.jsx
@@ -0,0 +1,22 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { Label } from './Filter.styled';
+
+import { getVisibleContact } from 'redux/filterSlice/filterSlice';
+
+export function Filter() {
+  const filter = useSelector(state => state.filterValue);
+  const dispatch = useDispatch();
+
+  return (
+    <>
+      <Label>
+        Find contacts by name
+        <input
+          type="text"
+          value={filter}
+          onChange={e => dispatch(getVisibleContact(e.target.value))}
+        />
+      </Label>
+    </>
+  );
+}
